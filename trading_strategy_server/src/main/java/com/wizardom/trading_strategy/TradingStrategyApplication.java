e2ac1b69e065072997e3d0aca370ddd47d9b377b
Ibekason Alexander <alibekason@gmail.com>
2021-06-02 21:03:39 +0100

    Initial commit

---
+++ trading_strategy_server/src/main/java/com/wizardom/trading_strategy/TradingStrategyApplication.java
@@ -0,0 +1,13 @@
+package com.wizardom.trading_strategy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradingStrategyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradingStrategyApplication.class, args);
+	}
+
+}
---
+++ trading_strategy_server/src/test/java/com/wizardom/trading_strategy/TradingStrategyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wizardom.trading_strategy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradingStrategyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
