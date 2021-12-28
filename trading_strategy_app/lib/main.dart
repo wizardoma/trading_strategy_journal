84ff28cd506b408a029812d51f44d1555e6535e2
Ibekason Alexander <alibekason@gmail.com>
2021-12-28 13:22:26 +0100

    Add needed packages

--- trading_strategy_app/lib/main.dart
+++ trading_strategy_app/lib/main.dart
@@ -1,16 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:http_code_util/http_code_util.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatelessWidget with StatusCodeUtil {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Trading Strategy"),
+          title: Text("Trade Book"),
         ),
       ),
     );
